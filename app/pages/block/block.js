17f5656e9b6a7336f9b85567d5183bb1324d1442
akira-cn <akira.cn@gmail.com>
2018-08-21 12:29:14 +0800

    add example

---
+++ app/pages/block/block.js
@@ -0,0 +1,32 @@
+// const app = getApp()
+const spritejs = require('../../lib/spritejs')
+const {Scene, Sprite} = spritejs
+
+Page({
+  data: {
+    layers: ['fglayer'],
+  },
+  onReady() {
+    const scene = new Scene()
+    const width = 750,
+      height = 1344
+
+    const layer = scene.layer('fglayer')
+    layer.append(new Sprite({
+      pos: [0, 0],
+      size: [100, 100],
+      bgcolor: 'red',
+      fillColor: '#29AB63',
+      borderRadius: 0.2,
+    }))
+
+    const s = new Sprite()
+    s.attr({
+      anchor: 0.5,
+      pos: [width / 2, height / 2],
+      size: [100, 100],
+      bgcolor: 'red',
+    })
+    layer.append(s)
+  },
+})
--- app/pages/index/index.js
+++ app/pages/index/index.js
@@ -7,6 +7,7 @@ Page({
     demos: [
       {url: '/pages/robot/robot', label: '罐罐动画'},
       {url: '/pages/water/water', label: '十滴水小游戏'},
+      {url: '/pages/block/block', label: '方块'},
     ],
   },
   onReady() {
